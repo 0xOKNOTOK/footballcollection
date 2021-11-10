3f2cafe990ec54c2e8167a4ca2117aa3b85bb636
Ross Greenall <rossgreenall@gmail.com>
2021-11-10 01:35:13 +0000

    Initialized CRA with Tailwind

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex flex-col w-3/4 mx-auto my-12 items-center'>
+      <h1>Super cool page</h1>
+    </div>
+  )
+}
+
+export default App
