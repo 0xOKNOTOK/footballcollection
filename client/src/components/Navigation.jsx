11e847a2be61b98b7c6e512e52069175b4519d10
Ross Greenall <rossgreenall@gmail.com>
2021-11-11 00:32:16 +0000

    Updated light theme, added dark theme for Navigation, LandingPage and Footer

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navigation = ({ handleSearchClick }) => {
   return (
-    <nav class='bg-gray-800 shadow light:bg-white'>
+    <nav class=' bg-gray-200 shadow dark:bg-gray-800'>
       <div class='container px-5 py-6 mx-auto md:flex'>
         <div class='flex items-center justify-between'>
           <div>
             <a
-              class='text-2xl font-bold text-white  light:text-gray-800 lg:text-3xl hover:text-gray-300 light:hover:text-gray-700'
+              class='text-2xl font-bold text-gray-800  dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300'
               href='#'
             >
               <svg
@@ -67,7 +67,7 @@ const Navigation = ({ handleSearchClick }) => {
           <div class='flex md:hidden'>
             <button
               type='button'
-              class=' text-gray-200 light:text-gray-500 hover:text-gray-400 light:hover:text-gray-600 focus:outline-none focus:text-gray-400 dark:focus:text-gray-600'
+              class=' text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400'
               aria-label='toggle menu'
             >
               <svg viewBox='0 0 24 24' class='w-6 h-6 fill-current'>
@@ -84,19 +84,19 @@ const Navigation = ({ handleSearchClick }) => {
           <div class='flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0'>
             <a
               href='#'
-              class='px-2 py-1 text-sm font-medium text-gray-200 transition-colors duration-200 transform rounded light:text-gray-700 hover:bg-gray-900 hover:text-gray-100 md:mx-2'
+              class='px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-700 hover:text-gray-100 md:mx-2'
             >
               Home
             </a>
             <a
               href='#'
-              class='px-2 py-1 text-sm font-medium text-gray-200 transition-colors duration-200 transform rounded light:text-gray-700 hover:bg-gray-900 hover:text-gray-100 md:mx-2'
+              class='px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-700 hover:text-gray-100 md:mx-2'
             >
               About
             </a>
             <a
               href='#'
-              class='px-2 py-1 text-sm font-medium text-gray-200 transition-colors duration-200 transform rounded light:text-gray-700 hover:bg-gray-900 hover:text-gray-100 md:mx-2'
+              class='px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-700 hover:text-gray-100 md:mx-2'
             >
               Contact
             </a>
@@ -122,7 +122,7 @@ const Navigation = ({ handleSearchClick }) => {
             <input
               onChange={handleSearchClick}
               type='text'
-              class='w-full py-2 pl-10 pr-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-red-500 dark:focus:border-red-500 focus:outline-none '
+              class='w-full py-2 pl-10 pr-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-grey-500 dark:focus:border-red-500 focus:outline-none focus:ring'
               placeholder='Search'
             />
           </div>
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,13 +4,13 @@ import Footer from '../components/Footer'
 const LandingPage = () => {
   return (
     <main class=''>
-      <section class='h-screen text-gray-400 bg-gray-900 body-font flex items-center flex-row'>
+      <section class='h-screen bg-gray-100 text-gray-400 dark:bg-gray-900 body-font flex items-center flex-row'>
         <div class='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
           <div class='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
-            <h1 class='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
+            <h1 class='title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-800 dark:text-white'>
               Football, visualised.
             </h1>
-            <p class='mb-8 leading-relaxed'>
+            <p class='mb-8 text-gray-600 dark:text-gray-300 leading-relaxed'>
               Football Collection (FC) aims to provide up-to-date statistics on
               all the latest EPL and UCL fixtures, players and teams in an easy
               to digest format.
