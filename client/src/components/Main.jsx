cf2316de27894bc3959dc89d7258be64d2b99004
Ross Greenall <rossgreenall@gmail.com>
2021-11-11 21:36:37 +0000

    MainComponent added, form component added, fixture component added.

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MainComponent = ({ children }) => {
+  return <main class='h-screen bg-gray-100 dark:bg-gray-900'>{children}</main>
+}
+
+export default MainComponent
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import MainComponent from '../components/Main'
 
 const LandingPage = () => {
   return (
-    <main>
+    <MainComponent>
       <section class='h-screen bg-gray-100 text-gray-400 dark:bg-gray-900 body-font flex items-center flex-row'>
         <div class=' container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
           <div class='lg:w-screen md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
@@ -34,7 +35,7 @@ const LandingPage = () => {
         </div>
       </section>
       <Footer />
-    </main>
+    </MainComponent>
   )
 }
 
