53378f0dc6bc1b7ea509ac1604992a779f40cbe0
Ross Greenall <rossgreenall@gmail.com>
2021-11-13 17:18:24 +0000

    Reorganised API data format.

--- server/controllers/teams.js
+++ server/controllers/teams.js
@@ -1,11 +1,20 @@
 // TEMP LOCAL DB
-
+const allClubs = require('../data/allClubs.json')
 const premierLeagueClubs = require('../data/premierLeague.json')
 
 // GET [ALL TEAMS FROM SUPPORTED LEAGUES]
 exports.getAllTeams = (req, res) => {
   try {
-    res.status(200).json()
+    res.status(200).json(allClubs)
+  } catch (error) {
+    res.status(500).json({ data: 'fail' })
+  }
+}
+
+// GET [ALL TEAMS FROM LEAGUE]
+exports.getTeamsByLeague = (req, res) => {
+  try {
+    res.status(200).json({})
   } catch (error) {
     res.status(500).json({ data: 'fail' })
   }
