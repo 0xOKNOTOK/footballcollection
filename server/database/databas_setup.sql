660b0ce397c3ebb8a4cc0bf189b183cdc3e08d99
Ross Greenall <rossgreenall@gmail.com>
2021-11-14 16:56:12 +0000

    Example structure generated for PostgreSQL

---
+++ server/database/databas_setup.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE footballcollection;
+
+-- \c into footballcollection database.
+
+CREATE TABLE teams(
+    team_id SERIAL PRIMARY KEY,
+
+
+)
+
+CREATE TABLE fixtures(
+    fixture_id SERIAL PRIMARY KEY,
+
+)
+
+CREATE TABLE players (
+    player_id SERIAL PRIMARY KEY,
+
+
+)
