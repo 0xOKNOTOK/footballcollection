bfe1c9b3416feb7886aa41851db516e6acb4d1a6
Ross Greenall <rossgreenall@gmail.com>
2021-11-14 18:55:30 +0000

    Changed table structure to use JSONB instead of individual fields.

--- server/database/databas_setup.sql
+++ server/database/databas_setup.sql
@@ -2,19 +2,12 @@ CREATE DATABASE footballcollection;
 
 -- \c into footballcollection database.
 
-CREATE TABLE teams(
-    team_id SERIAL PRIMARY KEY,
-
-
-)
+CREATE TABLE teams (
+  team_id integer NOT NULL,
+  data jsonb
+);
 
 CREATE TABLE fixtures(
     fixture_id SERIAL PRIMARY KEY,
-
-)
-
-CREATE TABLE players (
-    player_id SERIAL PRIMARY KEY,
-
 
 )
