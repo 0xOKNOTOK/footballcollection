c7b29802d0635a71346eb4f1f22d72d4fd160f7e
Ross Greenall <rossgreenall@gmail.com>
2021-11-16 23:09:42 +0000

    Changed badge size. TODO Fix collapse on smaller devices

--- client/src/components/Teams/TeamsCard.jsx
+++ client/src/components/Teams/TeamsCard.jsx
@@ -4,7 +4,7 @@ const TeamsCard = ({ name, badge }) => {
   console.log(badge)
   return (
     <div className='flex-grow w-screen md:pl-8 pl-6 flex sm:items-center items-start flex-row'>
-      <img src={badge} alt='TEMP' width='30px' />
+      <img src={badge} alt='TEMP' width='25px' />
       <div className='flex-grow sm:pl-6 mt-6 sm:mt-0'>
         <span className='font-medium title-font text-gray-900 dark:text-gray-200 mb-1 text-xl'>
           <a href='/clubpage' className=''>
