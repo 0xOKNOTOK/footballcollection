23b4499e4afecec6b56cd6a5d7b7de38e05901bf
Ross Greenall <rossgreenall@gmail.com>
2021-11-16 22:43:50 +0000

    Teams now show in correct position, with correct number by their names. Need to add data for the other seasons.

--- client/src/pages/TablesPage.jsx
+++ client/src/pages/TablesPage.jsx
@@ -8,19 +8,23 @@ import TeamsPosition from '../components/Teams/TeamsPosition'
 import FixtureContext from '../hooks/fixtureDataContext'
 
 const TeamsPage = () => {
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState('2021/22')
   const [table, setTable] = useState([])
 
   const fixturesData = useContext(FixtureContext)
 
   useEffect(() => {
-    setTable(fixturesData.data.leagues.PremierLeague.twentyone)
+    const Filtered = fixturesData.data.leagues.PremierLeague
+    setTable(
+      Filtered.sort((a, b) => {
+        return a.position - b.position
+      })
+    )
   }, [fixturesData])
 
   const handleFilterChange = e => {
     e.preventDefault()
     setFilter(e.target.value)
-    console.log(table)
   }
 
   return (
@@ -28,12 +32,27 @@ const TeamsPage = () => {
       <section className='text-gray-600 bg-white dark:bg-gray-900 body-font'>
         <div className='container px-5 py-24 mx-auto flex-col align-center justify-items-center flex flex-wrap'>
           <Dropdown handleFilterChange={handleFilterChange} />
-
-          {table.map((team, index) => (
-            <TeamsPosition key={index} position={index + 1}>
-              <TeamsCard name={team.name} badge={team.badge} />
-            </TeamsPosition>
-          ))}
+          {filter
+            ? table
+                .filter(table => table.year === filter)
+                .map((year, index) =>
+                  year.table
+                    .sort((a, b) => {
+                      return a.position - b.position
+                    })
+                    .map(team => {
+                      return (
+                        <TeamsPosition position={team.position}>
+                          <TeamsCard name={team.name} badge={team.badge} />
+                        </TeamsPosition>
+                      )
+                    })
+                )
+            : table.map((team, index) => (
+                <TeamsPosition key={index} position={index + 1}>
+                  <TeamsCard name={team.name} badge={team.badge} />
+                </TeamsPosition>
+              ))}
         </div>
       </section>
       <Footer />
